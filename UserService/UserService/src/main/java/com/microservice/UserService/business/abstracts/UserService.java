639def3a10b9cd140082fa972b232e62afac7d3b
Oguzhan Yıldız <oguzy2020@gmail.com>
2023-05-16 15:14:44 +0300

    added javadoc properties

--- UserService/UserService/src/main/java/com/microservice/UserService/business/abstracts/UserService.java
+++ UserService/UserService/src/main/java/com/microservice/UserService/business/abstracts/UserService.java
@@ -9,10 +9,45 @@ import com.microservice.UserService.entity.User;
 
 public interface UserService {
 
-	
+
+	/**
+	 * Adds a new user based on the provided addUserRequest.
+	 *
+	 * @param addUserRequest The request object containing the information of the user to be added.
+	 * @return A String representing the result or status of the user addition process.
+	 */
 	String addUser(AddUserRequest addUserRequest);
+	
+	/**
+	 * Updates an existing user based on the provided updateUserRequest.
+	 *
+	 * @param updateUserRequest The request object containing the updated information of the user.
+	 * @return A String representing the result or status of the user update process.
+	 */
 	String updateUser(UpdateUserRequest updateUserRequest);
+	
+	/**
+	 * Retrieves a user by their ID.
+	 *
+	 * @param id The ID of the user to retrieve.
+	 * @return The User object representing the user with the specified ID, or null if no user is found.
+	 */
 	User getUserById(int id);
+	
+	/**
+	 * Soft deletes a user by their ID.
+	 *
+	 * @param userId The ID of the user to be soft deleted.
+	 */
 	void softDeleteUserById(int userId);
+	/**
+	 * Retrieves a paginated list of vehicles.
+	 *
+	 * @param no             The page number to retrieve.
+	 * @param size           The number of items to include per page.
+	 * @param sortBy         The field to sort the results by.
+	 * @param sortDirection  The direction of the sorting (ascending or descending).
+	 * @return A Page object containing the requested list of vehicles.
+	 */
 	Page<UserResponse> getVehicles(int no, int size, String sortBy, String sortDirection);
 }
