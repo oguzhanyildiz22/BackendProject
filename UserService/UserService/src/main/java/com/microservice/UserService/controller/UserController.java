52afe47b7b6e94d60347904674dd5559c485b726
Oguzhan Yıldız <oguzy2020@gmail.com>
2023-05-16 17:12:13 +0300

    Added logging process for UserController class

--- UserService/UserService/src/main/java/com/microservice/UserService/controller/UserController.java
+++ UserService/UserService/src/main/java/com/microservice/UserService/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.microservice.UserService.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -29,62 +31,89 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
+	private static final Logger logger = LogManager.getLogger(UserController.class);
+
 	@GetMapping("/admin")
 	public String admin(@RequestHeader("Authorization") String authorizationHeader)
 			throws JsonMappingException, JsonProcessingException {
 
+		logger.info("Admin endpoint called with authorization header: {}", authorizationHeader);
+
 		if (userService.checkRole(authorizationHeader)) {
+			logger.info("User has ADMIN authority");
 			return "hello admin";
 		} else {
+			logger.warn("User does not have ADMIN authority");
 			return "You do not have ADMIN authority";
 		}
 	}
 
-	
 	@PostMapping("/add")
-	public String add(@RequestBody AddUserRequest addUserRequest,@RequestHeader("Authorization") String authorizationHeader) {
-		
-		if(userService.checkRole(authorizationHeader)) {
-			this.userService.addUser(addUserRequest);
-			
-			return "added";
-		}
-		return "did not add";
-		
-	}
-	@PutMapping("/update")
-	public String update(@RequestBody UpdateUserRequest updateUserRequest,@RequestHeader("Authorization") String authorizationHeader) {
-		 
+	public String add(@RequestBody AddUserRequest addUserRequest,
+			@RequestHeader("Authorization") String authorizationHeader) {
+
+		logger.info("Add endpoint called with authorization header: {}", authorizationHeader);
+
 		if (userService.checkRole(authorizationHeader)) {
+			logger.info("User has ADMIN authority. Adding user: {}", addUserRequest);
+			this.userService.addUser(addUserRequest);
+			return "added";
+		} else {
+			logger.warn("User does not have ADMIN authority. User not added.");
+			return "did not add";
+		}
+
+	}
+
+	@PutMapping("/update")
+	public String update(@RequestBody UpdateUserRequest updateUserRequest,
+			@RequestHeader("Authorization") String authorizationHeader) {
+
+		logger.info("Update endpoint called with authorization header: {}", authorizationHeader);
+
+		if (userService.checkRole(authorizationHeader)) {
+			logger.info("User has ADMIN authority. Updating user: {}", updateUserRequest);
 			this.userService.updateUser(updateUserRequest);
 			return "user updated";
+		} else {
+			logger.warn("User does not have ADMIN authority. User not updated.");
+			return "user did not update";
 		}
-		
-		return "user did not update";
-		
+
 	}
-	
+
 	@DeleteMapping("/delete/{id}")
-    public String softDeleteUserById(@PathVariable("id") int userId,@RequestHeader("Authorization") String authorizationHeader) {
-       
+	public String softDeleteUserById(@PathVariable("id") int userId,
+			@RequestHeader("Authorization") String authorizationHeader) {
+
+		logger.info("Delete endpoint called with authorization header: {}", authorizationHeader);
+
 		if (userService.checkRole(authorizationHeader)) {
+			logger.info("User has ADMIN authority. Soft deleting user with ID: {}", userId);
 			userService.softDeleteUserById(userId);
-	        return "deleted";
+			return "deleted";
+		} else {
+			logger.warn("User does not have ADMIN authority. User not deleted.");
+			return "did not delete";
 		}
-		return "did not deleted";
-    }
-	
+	}
+
 	@GetMapping("/page")
 	public ResponseEntity<Page<UserResponse>> getUsers(@RequestHeader("Authorization") String authorizationHeader,
-			@RequestParam int no,
-			@RequestParam int size,
-			@RequestParam String sortBy,
-			@RequestParam String sortDirection){
+			@RequestParam int no, @RequestParam int size, @RequestParam String sortBy,
+			@RequestParam String sortDirection) {
+
+		logger.info("GetUsers endpoint called with authorization header: {}", authorizationHeader);
+
 		if (userService.checkRole(authorizationHeader)) {
-			Page<UserResponse> users = userService.getUsers(no, size,sortBy,sortDirection);
+			logger.info(
+					"User has ADMIN authority. Fetching users with pagination parameters: no={}, size={}, sortBy={}, sortDirection={}",
+					no, size, sortBy, sortDirection);
+			Page<UserResponse> users = userService.getUsers(no, size, sortBy, sortDirection);
 			return new ResponseEntity<>(users, HttpStatus.OK);
+		} else {
+			logger.warn("User does not have ADMIN authority. Access unauthorized.");
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 		}
-		
-		return new ResponseEntity<Page<UserResponse>>(HttpStatus.UNAUTHORIZED);
 	}
 }
