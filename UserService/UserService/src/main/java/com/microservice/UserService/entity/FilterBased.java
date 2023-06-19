7d7b0ea2e9fc188f20f1ee51f8f9ca5f2b6e8fa5
Oğuzhan YILDIZ <110741518+oguzhanyildiz22@users.noreply.github.com>
2023-06-19 13:56:47 +0300

    Update FilterBased.java

--- UserService/UserService/src/main/java/com/microservice/UserService/entity/FilterBased.java
+++ UserService/UserService/src/main/java/com/microservice/UserService/entity/FilterBased.java
@@ -1,16 +1,27 @@
 package com.microservice.UserService.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "filter_baseds")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class FilterBased {
 
-	private String filterCode;
-    private int linkCount;
-    private String userDesc;
-    private String termName;
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+
+	    private String filterCode;
+	    private int linkCount;
+	    private String userDesc;
+	    private String termName;
 }
