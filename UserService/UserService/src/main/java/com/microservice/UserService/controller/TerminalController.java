33b729135aabf8c08b29c150d33fb2a9be2ffe67
Oğuzhan YILDIZ <110741518+oguzhanyildiz22@users.noreply.github.com>
2023-06-19 13:55:42 +0300

    Update TerminalController.java

--- UserService/UserService/src/main/java/com/microservice/UserService/controller/TerminalController.java
+++ UserService/UserService/src/main/java/com/microservice/UserService/controller/TerminalController.java
@@ -1,56 +1,28 @@
 package com.microservice.UserService.controller;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.microservice.UserService.entity.MyObject;
+import com.microservice.UserService.entity.TerminalData;
+import com.microservice.UserService.repository.TerminalDataRepository;
 
 @RestController
 @RequestMapping("api/terminal")
 public class TerminalController {
+    private final TerminalDataRepository terminalDataRepository;
 
-	@GetMapping("/data")
-	public MyResponse getData() throws IOException {
-	    InputStream inputStream = getClass().getResourceAsStream("/terminals.json");
-	    ObjectMapper objectMapper = new ObjectMapper();
-	    MyResponse response = objectMapper.readValue(inputStream, MyResponse.class);
-	    return new MyResponse("SUCCESS", response.getData());
-	}
+    @Autowired
+    public TerminalController(TerminalDataRepository terminalDataRepository) {
+        this.terminalDataRepository = terminalDataRepository;
+    }
 
-	public static class MyResponse {
-	    private String type;
-	    private List<MyObject> data;
-
-	    public MyResponse(String type, List<MyObject> data) {
-	        this.type = type;
-	        this.data = data;
-	    }
-	    
-	    public MyResponse() {
-	       
-	    }
-
-	    public String getType() {
-	        return type;
-	    }
-
-	    public void setType(String type) {
-	        this.type = type;
-	    }
-
-	    public List<MyObject> getData() {
-	        return data;
-	    }
-
-	    public void setData(List<MyObject> data) {
-	        this.data = data;
-	    }
-	}
-    
+    @GetMapping("/data")
+    public List<TerminalData> getAllTerminals() {
+        return terminalDataRepository.findAll();
+    }
 }
+
